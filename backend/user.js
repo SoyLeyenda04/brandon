d965c0ef6c95e18ace520cf3fc1cc7cb0a62caca
Brandon Corrales <brandon2021cuenta@gmail.com>
2025-02-08 07:49:24 -0600

    Create user.js

---
+++ backend/user.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+router.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.status(201).send('User registered');
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email, password });
+    if (user) {
+        res.status(200).send('Login successful');
+    } else {
+        res.status(400).send('Invalid credentials');
+    }
+});
+
+module.exports = router;
